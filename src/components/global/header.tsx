db9a7a1d038d9d4a71ea37b887a4409c5a2dccb1
Re9iNee <attarzadeh76@gmail.com>
2023-11-19 23:04:39 +0330

    header redesign to support MD screens

--- src/components/global/header.tsx
+++ src/components/global/header.tsx
@@ -2,22 +2,35 @@ import { FaMedium } from "react-icons/fa6";
 import { AiOutlineMediumWorkmark } from "react-icons/ai";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
+import Link from "next/link";
 
 function Header() {
   return (
-    <div className='bg-amber-400' id='header'>
-      <header className='flex justify-between p-4 items-center'>
-        <section className='inline-flex gap-2 cursor-pointer text-6xl text-black'>
+    <header className='bg-amber-400 ' id='header'>
+      <nav className='inline-flex p-4 gap-2 items-center justify-between w-full text-black'>
+        <Link href={"/"} className='inline-flex gap-2 text-6xl'>
           <FaMedium />
           <AiOutlineMediumWorkmark />
-        </section>
+        </Link>
 
-        <Button variant={"outline"} className='rounded-3xl'>
-          Get Started
-        </Button>
-      </header>
+        <div className='inline-flex gap-8 items-center'>
+          <Link href={"/about"} className='hidden md:block hover:text-gray-300'>
+            About
+          </Link>
+          <Link
+            href={"/contact"}
+            className='hidden md:block hover:text-gray-300'
+          >
+            Contact
+          </Link>
+
+          <Button variant={"outline"} className='rounded-3xl text-gray-200'>
+            Get Started
+          </Button>
+        </div>
+      </nav>
       <Separator />
-    </div>
+    </header>
   );
 }
 
