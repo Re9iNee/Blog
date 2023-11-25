20cfe05ddcdf03d7cb800c264a47f424a7a3def7
Re9iNee <attarzadeh76@gmail.com>
2023-11-25 15:35:33 +0330

    Adding db seeder using fakerjs

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,72 @@
-import { PrismaClient } from "@prisma/client";
+import { Post, PrismaClient, User } from "@prisma/client";
+import { faker } from "@faker-js/faker";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 const prisma = new PrismaClient().$extends(withAccelerate());
 
 async function main() {
-  const result = await prisma.post.create({
-    data: {
-      author: { connect: { id: 1 } },
-      title: "My first post",
-      summery: "This is my first post",
-      readingTime: 5,
-      body: "This is the body of my first post",
-      published: true,
-      publishedAt: new Date(),
-      status: "published",
-    },
+  // await insertUsers();
+  // await insertPosts();
+  // await getAllAuthors();
+}
+
+async function getAllAuthors() {
+  console.log("Fetching all authors...");
+
+  const authors = await prisma.user.findMany({
+    include: { posts: true },
   });
 
-  console.log(result);
+  console.dir(authors, { depth: null });
+  return authors;
+}
+
+async function insertUsers() {
+  console.log(`inserting users...`);
+
+  const users = [];
+  for (let i = 0; i < 3; i++) {
+    const newUser = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      avatarUrl: faker.image.avatar(),
+    };
+
+    users.push(newUser);
+  }
+
+  const result = await prisma.user.createMany({ data: users.slice() });
+
+  console.log(`inserted to DB: `, result);
+  return result;
+}
+
+async function insertPosts() {
+  console.log(`inserting posts...`);
+
+  const users = await prisma.user.findMany();
+
+  const posts = [];
+  for (let i = 0; i < 10; i++) {
+    const randomAuthor = users[Math.floor(Math.random() * users.length)];
+
+    const result = await prisma.post.create({
+      data: {
+        title: faker.lorem.sentence(),
+        summery: faker.lorem.paragraph(),
+        readingTime: faker.number.int({ max: 30 }),
+        body: faker.lorem.paragraphs(10),
+        mainImageUrl: faker.image.urlPicsumPhotos(),
+        published: faker.datatype.boolean(),
+        author: { connect: { id: randomAuthor.id } },
+      },
+    });
+
+    posts.push(result);
+  }
+
+  console.log(`inserted to DB: `, posts);
+  return posts;
 }
 
 main()
