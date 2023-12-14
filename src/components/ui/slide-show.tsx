88fbcdf1cd1b3bd82540d4baae4f6875c3c421cd
Re9iNee <attarzadeh76@gmail.com>
2023-12-14 13:46:26 +0330

    finished slideshow content on xs

--- src/components/ui/slide-show.tsx
+++ src/components/ui/slide-show.tsx
@@ -26,23 +26,28 @@ export function SlideShow({ className, cards }: Props) {
 
   return (
     // container
-    <div className={cn("grid grid-cols-12 grid-rows-5 h-80", className)}>
+    <div
+      className={cn(
+        "grid grid-cols-6 md:grid-cols-12 grid-rows-5 h-80",
+        className
+      )}
+    >
       {/* left button */}
       <a
         onClick={() => paginate(-1)}
-        className={`col-start-1 col-span-1 row-start-3 pr-0.5 slideshow-action z-20 place-self-center`}
+        className={`col-start-1 col-span-1 row-start-3 pr-0.5 slideshow-action z-20 place-self-center justify-self-center`}
       >
         <IoChevronBack />
       </a>
 
-      <section className='col-start-1 col-span-full row-start-1 row-span-full'>
+      <section className='col-start-1 col-span-12 row-start-1 row-span-full'>
         <SlideShowCard {...cards[page]} direction={direction} page={page} />
       </section>
 
       {/* right button */}
       <a
         onClick={() => paginate(1)}
-        className={`col-start-12 col-span-1 row-start-3 pl-0.5 slideshow-action z-20 place-self-center`}
+        className={`col-start-6 md:col-start-12 col-span-1 row-start-3 pl-0.5 slideshow-action z-20 place-self-center justify-self-center`}
       >
         <IoChevronForward />
       </a>
@@ -104,7 +109,9 @@ export function SlideShowCard({
         custom={direction}
         variants={variants}
         transition={{ duration: 0.4 }}
-        className={cn("relative grid grid-cols-12 grid-rows-6 h-full")}
+        className={cn(
+          "relative grid grid-cols-6 md:grid-cols-12 grid-rows-6 min-h-full"
+        )}
       >
         <Image
           width={96}
@@ -121,7 +128,30 @@ export function SlideShowCard({
           className='z-0 pointer-events-none rounded-3xl'
         />
 
-        <section className='col-span-full row-start-4 row-span-full z-10 flex justify-between gap-11 items-end px-4 pb-4'>
+        {/* only xs */}
+        <h5 className='bg-neutral-800 px-3 text-white py-1 rounded-xl bg-opacity-40 text-xs font-light leading-none col-start-5 col-span-2 z-10 max-h-6 grid place-items-center self-center justify-self-start md:hidden'>
+          {category}
+        </h5>
+
+        {/* only xs */}
+        <section
+          className='col-span-full row-start-4 row-span-full z-10 flex flex-col gap-3 justify-end px-2
+          md:hidden
+          '
+        >
+          <h3 className='text-white font-bold'>{title}</h3>
+          <h6 className='text-neutral-400 text-xs font-light leading-none'>
+            By {author}
+          </h6>
+          <button className='group inline-flex border whitespace-nowrap bg-neutral-800 bg-opacity-0 rounded-3xl py-2 pl-8 pr-6 items-center justify-center hover:bg-opacity-30 hover:px-6 transition-all mt-3 mb-2'>
+            <span className='text-white text-sm font-medium group-active:scale-85 duration-500 origin-center'>
+              Start Reading
+            </span>
+            <ArrowIcon className='opacity-0 w-2 h-5 text-white group-hover:opacity-100 group-hover:ml-2 text-xl group-active:scale-75 duration-500 pt-0.5' />
+          </button>
+        </section>
+
+        <section className='hidden col-span-full row-start-4 row-span-full z-10 md:flex justify-between gap-11 items-end px-4 pb-4'>
           <div className='flex flex-col justify-end items-start gap-2 text-white'>
             <h5 className='bg-neutral-800 px-3 py-1 rounded-xl bg-opacity-40 text-xs font-light leading-none'>
               {category}
