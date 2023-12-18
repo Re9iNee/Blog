0a96a35664f2800c9fb7c18fd2e17677109b224a
Re9iNee <attarzadeh76@gmail.com>
2023-12-18 14:22:18 +0330

    Header on all screen sizes

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,9 +1,17 @@
 import Footer from "@/components/global/footer";
 import Header from "@/components/global/header";
+import { cn } from "@/lib/utils";
+
+import AnuratiRegular from "next/font/local";
+
+const logoFont = AnuratiRegular({
+  src: "./fonts/Anurati-Regular.otf",
+  variable: "--font-anurati",
+});
 
 function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className='max-w-screen-xl mx-auto'>
+    <div className={`max-w-screen-xl mx-auto ${logoFont.variable}`}>
       <Header />
       {children}
       <Footer />
