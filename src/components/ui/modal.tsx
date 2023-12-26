9402368b1589f9558f957fca5f2ff98cd7c7ada8
Re9iNee <attarzadeh76@gmail.com>
2023-12-26 23:42:28 +0330

    fix-dismissing modal

--- src/components/ui/modal.tsx
+++ src/components/ui/modal.tsx
@@ -24,7 +24,7 @@ export default function Modal({
   return (
     <NextUiModal
       isOpen={isOpen}
-      isDismissable={false}
+      isDismissable={true}
       placement='top-center'
       onOpenChange={onOpenChange}
     >
