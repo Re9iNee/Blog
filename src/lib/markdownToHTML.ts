2316247d20636b6a3021485f2146244f8c94076c
Re9iNee <attarzadeh76@gmail.com>
2024-01-06 17:27:20 +0330

    adding typing for mark handle function

--- src/lib/markdownToHTML.ts
+++ src/lib/markdownToHTML.ts
@@ -1,6 +1,7 @@
 import { micromark } from "micromark";
 import {
   Directive,
+  Handle,
   directive,
   directiveHtml,
 } from "micromark-extension-directive";
@@ -15,11 +16,13 @@ export async function markdownToHTML(content: string): Promise<string> {
   return file;
 }
 
-function mark(d: Directive) {
+const mark: Handle = function (d: Directive) {
   if (d.type !== "textDirective") return false;
 
   this.tag("<mark");
   this.tag(">");
   this.raw(d.label || "");
   this.tag("</mark>");
-}
+
+  return undefined;
+};
