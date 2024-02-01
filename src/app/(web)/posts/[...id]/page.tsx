2821d68ddbd9c4e2bcf639f4141c4f96ac8474ed
Re9iNee <attarzadeh76@gmail.com>
2024-02-01 18:11:02 +0330

    setting noStore function back

--- src/app/(web)/posts/[...id]/page.tsx
+++ src/app/(web)/posts/[...id]/page.tsx
@@ -11,12 +11,14 @@ import ShareIcon from "public/icons/Share.svg";
 import { RxDotFilled } from "react-icons/rx";
 
 import ClapContainer from "@/components/posts/claps/clap-container";
+import { unstable_noStore as noStore } from "next/cache";
 
 type Props = {
   params: { id: string };
 };
 
 async function PostPage({ params }: Props) {
+  noStore();
   const id = params.id;
 
   const data = await getPost(+id);
