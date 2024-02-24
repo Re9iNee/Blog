df5d6a6dd8332a36f7131bff2fb42c446e64c1ee
Re9iNee <attarzadeh76@gmail.com>
2024-02-25 00:45:27 +0330

    Feature-add skeleton loading for each posts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -66,3 +66,7 @@ export function getS3ObjectURLFromKey(key: string) {
 export function getRandomInt(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function wait(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
