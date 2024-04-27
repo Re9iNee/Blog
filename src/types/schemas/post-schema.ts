eaa966db60cc627acd629489c9ee3c84af2ced44
Re9iNee <attarzadeh76@gmail.com>
2024-04-27 13:21:45 +0330

    Feature-post title char limit 80, and summary to 300

--- src/types/schemas/post-schema.ts
+++ src/types/schemas/post-schema.ts
@@ -3,12 +3,14 @@ import { z } from "zod";
 export const postSchema = z.object({
   id: z.number(),
   body: z.string(),
-  title: z.string(),
+  title: z
+    .string()
+    .max(80, { message: "Title should be less than 80 characters" }),
   summary: z
     .string({
       invalid_type_error: "Please write down a summary",
     })
-    .max(80, { message: "Summery should be less than 80 characters" }),
+    .max(300, { message: "Summery should be less than 80 characters" }),
   authorId: z.coerce.number({ invalid_type_error: "Please select an author" }),
   readingTime: z.coerce.number(),
   publishedAt: z.date().nullish(),
