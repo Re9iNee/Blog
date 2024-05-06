56643f1c34ed758d5dcb6f2c3b30ce524c613fb9
Re9iNee <attarzadeh76@gmail.com>
2024-05-06 13:44:22 +0330

    Fix-uploader component

--- src/components/ui/uploader.tsx
+++ src/components/ui/uploader.tsx
@@ -1,5 +1,8 @@
 import { Input } from "@/components/ui/input";
 import { uploadToS3 } from "@/service/upload.service";
+import { Loader2 } from "lucide-react";
+import { MouseEventHandler, useRef, useState } from "react";
+import { Button } from "./button";
 import { toast } from "./use-toast";
 
 type Props = {
@@ -7,8 +10,28 @@ type Props = {
   defaultValue?: string;
   onUploadFinished: (url: string) => void;
 };
-export function Uploader({ onUploadFinished, ...props }: Props) {
-  const changeHandler: ChangeEventHandler<HTMLInputElement> = (event) => {
+export function Uploader({
+  name,
+  defaultValue,
+  onUploadFinished,
+  ...props
+}: Props) {
+  const ref = useRef<HTMLInputElement>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const handleImageChange: MouseEventHandler<HTMLButtonElement> = async (
+    ev
+  ) => {
+    ev.preventDefault();
+    const files = ref.current?.files;
+
+    if (!files?.length) {
+      toast({ title: "No file selected" });
+      return;
+    }
+
+    setIsLoading(true);
+
     const formData = new FormData();
     formData.append("file", files[0]);
 
@@ -25,12 +48,31 @@ export function Uploader({ onUploadFinished, ...props }: Props) {
   };
 
   return (
-    <Input
-      type='file'
-      data-cy='uploader'
-      onChange={changeHandler}
-      accept='audio/*,video/*,image/*'
-      {...props}
-    />
+    <div className='flex flex-col gap-4'>
+      <input type='hidden' name={name} defaultValue={defaultValue} />
+      <Input
+        ref={ref}
+        type='file'
+        data-cy='uploader'
+        accept='audio/*,video/*,image/*'
+        {...props}
+      />
+      <Button
+        disabled={isLoading}
+        data-cy='upload-btn'
+        className='self-end'
+        variant={"secondary"}
+        onClick={handleImageChange}
+      >
+        {isLoading ? (
+          <>
+            <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+            Uploading...
+          </>
+        ) : (
+          "Upload"
+        )}
+      </Button>
+    </div>
   );
 }
