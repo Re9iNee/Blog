16326b3be1210c2cc27eac4ac4bd5ff2b1fe1126
Re9iNee <attarzadeh76@gmail.com>
2024-05-07 17:59:00 +0330

    Feature-404Page-xs

---
+++ src/lib/fonts.ts
@@ -0,0 +1,5 @@
+import { Outfit, Noto_Sans } from "next/font/google";
+// used weight: light
+export const outfit = Outfit({ subsets: ["latin"], weight: "300" });
+// used weight: semiBold
+export const notoSans = Noto_Sans({ weight: "600", subsets: ["latin"] });
