5068bbd9595d4c2cd6223c4fe0fd2c4f80b8b367
Re9iNee <attarzadeh76@gmail.com>
2024-05-22 21:33:52 +0330

    adding google code verification

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -3,7 +3,6 @@ import Header from "@/components/global/header";
 import { notoSans, poppins } from "@/lib/fonts";
 import { GoogleTagManager } from "@next/third-parties/google";
 import { Metadata } from "next";
-import { notFound } from "next/navigation";
 
 export const metadata: Metadata = {
   title: {
@@ -15,14 +14,16 @@ export const metadata: Metadata = {
 
 function layout({ children }: { children: React.ReactNode }) {
   const gtmId = process.env.GTMID;
-  if (!gtmId) {
-    throw new Error("GTMID is not defined");
+  const googleSiteVerification = process.env.GOOGLE_SITE_VERIFICATION;
+  if (!gtmId || !googleSiteVerification) {
+    throw new Error("GTMID OR google site verification code is not defined");
   }
 
   return (
     <div
       className={`max-w-screen-xl min-h-screen mx-auto flex flex-col ${notoSans.className} ${poppins.variable}`}
     >
+      <meta name='google-site-verification' content={googleSiteVerification} />
       <GoogleTagManager gtmId={gtmId} />
       <Header />
       <div className='flex-grow relative'>{children}</div>
