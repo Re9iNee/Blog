037a08b98f72fbcceeb4e7e4e13f9f867284dd46
Re9iNee <attarzadeh76@gmail.com>
2024-05-29 14:59:36 +0330

    Chips toggle active

--- src/components/ui/chips.tsx
+++ src/components/ui/chips.tsx
@@ -1,15 +1,19 @@
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 type Props = {
   children: string;
+  isActive?: boolean;
 };
-function Chip({ children }: Props) {
+function Chip({ children, isActive }: Props) {
   return (
     <Link
       href={`/tags/${children.toLowerCase()}`}
-      className='rounded-[32px] text-xs text-neutral-600 bg-neutral-50 px-4 py-1.5 
-      dark:bg-blue-950 dark:text-white
-      active:bg-gradient-to-tr active:to-[#9767FE] active:from-[#5F14FF] active:text-neutral-50'
+      className={cn(
+        "rounded-[32px] text-xs text-neutral-600 bg-neutral-50 px-4 py-1.5 dark:bg-blue-950 dark:text-white",
+        isActive &&
+          "bg-gradient-to-tr to-[#9767FE] from-[#5F14FF] text-neutral-50"
+      )}
     >
       {children}
     </Link>
