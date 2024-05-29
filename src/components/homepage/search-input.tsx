c498cc1465e000b1605fa7d13c9a29d3f8dabf9e
Re9iNee <attarzadeh76@gmail.com>
2024-05-29 11:52:49 +0330

    migrating whole search input into smaller component

--- src/components/homepage/search-input.tsx
+++ src/components/homepage/search-input.tsx
@@ -3,8 +3,12 @@
 import useSetURLParams from "@/hooks/useSetURLParams";
 import { cn } from "@/lib/utils";
 import { ClassValue } from "clsx";
+import Image from "next/image";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 import { useDebouncedCallback } from "use-debounce";
 import { Input } from "../ui/input";
+import EnterIcon from "public/icons/EnterIcon.svg";
+import LittleFlame from "public/images/little-flame.avif";
 
 export default function SearchInput({ className }: { className?: ClassValue }) {
   const { setToUrl } = useSetURLParams({ key: "query", defaultValue: "" });
@@ -14,11 +18,29 @@ export default function SearchInput({ className }: { className?: ClassValue }) {
   }, 500);
 
   return (
-    <Input
-      type='text'
-      className={cn(className)}
-      onChange={(e) => onChange(e.target.value)}
-      placeholder='Find your topic here...'
-    />
+    <div className={cn(className)}>
+      <Image
+        aria-hidden
+        src={LittleFlame}
+        placeholder={"empty"}
+        alt='little flame icon'
+        className='w-[57px] h-[67px] object-none absolute right-3 -top-[67px] transition-all group-hover:top-0 group-hover:scale-50'
+      />
+
+      <Input
+        type='text'
+        className={cn(
+          "pl-14 pr-20 text-md font-medium w-full min-h-14 shadow-xl rounded-2xl placeholder:text-neutral-400 relative"
+        )}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder='Find your topic here...'
+      />
+
+      <FaMagnifyingGlass className='w-6 h-6 absolute top-1/2 transform -translate-y-1/2 left-4 text-purple-600 pointer-events-none' />
+      <div className='absolute top-1/2 transform -translate-y-1/2 right-4 text-neutral-500 pointer-events-none items-center gap-1 text-xs font-bold flex'>
+        <EnterIcon />
+        Enter
+      </div>
+    </div>
   );
 }
--- src/components/homepage/search.tsx
+++ src/components/homepage/search.tsx
@@ -1,9 +1,6 @@
 import Image from "next/image";
 import StarImage from "public/icons/star.avif";
-import LittleFlame from "/public/images/little-flame.avif";
-import { FaMagnifyingGlass } from "react-icons/fa6";
 import Chip from "../ui/chips";
-import { Input } from "../ui/input";
 import SearchInput from "./search-input";
 
 export default function Search() {
@@ -27,22 +24,7 @@ export default function Search() {
         Find your topic <span className='text-purple-600 ml-1'>Faster</span>
       </h2>
 
-      <div className='relative w-4/6 max-w-[550px]'>
-        <Image
-          aria-hidden
-          src={LittleFlame}
-          placeholder={"empty"}
-          alt='little flame icon'
-          className='w-[57px] h-[67px] object-none absolute right-3 -top-[67px] transition-all group-hover:top-0 group-hover:scale-50'
-        />
-
-        <SearchInput className='pl-14 pr-20 text-md font-medium w-full min-h-14 shadow-xl rounded-2xl placeholder:text-neutral-400 relative' />
-        <FaMagnifyingGlass className='w-6 h-6 absolute top-1/2 transform -translate-y-1/2 left-4 text-purple-600 pointer-events-none' />
-        <div className='absolute top-1/2 transform -translate-y-1/2 right-4 text-neutral-500 pointer-events-none items-center gap-1 text-xs font-bold flex'>
-          <EnterIcon />
-          Enter
-        </div>
-      </div>
+      <SearchInput className='relative w-4/6 max-w-[550px]' />
 
       <section aria-label='trending topics' className='space-y-3'>
         <h3 className='text-neutral-600 font-bold text-sm'>Trending Topics</h3>
@@ -55,33 +37,5 @@ export default function Search() {
         </div>
       </section>
     </section>
-  );
-}
-
-function EnterIcon() {
-  return (
-    <svg
-      width='16'
-      height='16'
-      viewBox='0 0 16 16'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
-    >
-      <path
-        d='M5.99935 6.66748L2.66602 10.0008L5.99935 13.3341'
-        stroke='#737373'
-        style={{ stroke: "#737373" }}
-        strokeWidth='1.5'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-      <path
-        d='M13.3327 2.66748V7.33415C13.3327 8.04139 13.0517 8.71967 12.5516 9.21977C12.0515 9.71986 11.3733 10.0008 10.666 10.0008H2.66602'
-        stroke='#737373'
-        strokeWidth='1.5'
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-    </svg>
   );
 }
