2583225b9941f3908e3effb5a5e7325aaa27c96b
Re9iNee <attarzadeh76@gmail.com>
2024-06-07 16:09:21 +0330

    Fix-SearchInput Design changes

--- src/components/homepage/search-input.tsx
+++ src/components/homepage/search-input.tsx
@@ -11,13 +11,13 @@ import { useSearchParams } from "next/navigation";
 
 type Props = {
   className?: ClassValue;
-  isTyping: boolean;
-  setIsTyping: Dispatch<SetStateAction<boolean>>;
+  isFocused: boolean;
+  setIsFocused: Dispatch<SetStateAction<boolean>>;
 };
 export default function SearchInput({
   className,
-  isTyping,
-  setIsTyping,
+  isFocused,
+  setIsFocused,
 }: Props) {
   const inputRef = useRef<HTMLInputElement>(null);
   const searchParams = useSearchParams();
@@ -33,8 +33,6 @@ export default function SearchInput({
       { key: "page", value: "1" },
       { key: "query", value: query },
     ]);
-
-    setIsTyping(false);
   }, 500);
 
   // const clearQuery = () => {
@@ -54,8 +52,8 @@ export default function SearchInput({
         placeholder={"empty"}
         alt='little flame icon'
         className={cn(
-          "w-[57px] h-[67px] object-none absolute right-3 -top-[67px] transition-all",
-          isTyping && "top-0 scale-50"
+          "w-[57px] h-[67px] object-none absolute right-3 -top-[65px] transition-all",
+          isFocused && "top-0 scale-50"
         )}
       />
       <Input
@@ -63,10 +61,15 @@ export default function SearchInput({
         ref={inputRef}
         defaultValue={query ?? ""}
         className={cn(
-          "pl-14 pr-20 text-md font-medium w-full min-h-14 shadow-xl rounded-2xl placeholder:text-neutral-400 relative"
+          "pl-14 pr-20 text-md font-medium w-full min-h-14 shadow-xl rounded-2xl placeholder:text-neutral-400 relative active:shadow-2xl focus:shadow-2xl  focus-visible:ring-opacity-30 focus-visible:ring-black"
         )}
+        onFocus={() => {
+          setIsFocused(true);
+        }}
+        onBlur={() => {
+          setIsFocused(false);
+        }}
         onChange={(e) => {
-          setIsTyping(true);
           onChange(e.target.value);
         }}
         placeholder='Find your topic here...'
