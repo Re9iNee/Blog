a6e48e7b63c0433217ba05e62944bbf18d8b3e7b
Re9iNee <attarzadeh76@gmail.com>
2024-06-08 23:13:17 +0330

    Fix-typo in post schema validation

--- src/types/schemas/post-schema.ts
+++ src/types/schemas/post-schema.ts
@@ -22,7 +22,7 @@ export const postSchema = z.object({
     .string({
       invalid_type_error: "Please write down a summary",
     })
-    .max(300, { message: "Summery should be less than 80 characters" }),
+    .max(300, { message: "Summery should be less than 300 characters" }),
 
   // TODO
   // author: z.any().optional(),
