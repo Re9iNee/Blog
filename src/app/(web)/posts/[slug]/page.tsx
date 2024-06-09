2cae090ec1645f9d199026dc5686f28fcc1e01c2
Re9iNee <attarzadeh76@gmail.com>
2024-06-09 19:25:15 +0330

    Updating schema LD json fields

--- src/app/(web)/posts/[slug]/page.tsx
+++ src/app/(web)/posts/[slug]/page.tsx
@@ -44,11 +44,33 @@ export async function generateMetadata(
 
 function generateSchemaJson(post: PostModel) {
   const jsonLd = {
-    "@context": "https://schema.org",
-    "@type": "Blog",
     name: post.title,
-    image: post.mainImageUrl,
+    identifier: post.id,
+    headline: post.title,
+    "@type": "NewsArticle",
     description: post.summary,
+    image: [post.mainImageUrl],
+    creator: [post.author.name],
+    dateCreated: post.createdAt,
+    dateModified: post.updatedAt,
+    "@context": "https://schema.org",
+    url: "https://mora-ed.com/posts/" + post.slug,
+    datePublished: post.publishedAt ?? post.createdAt,
+    mainEntityOfPage: "https://mora-ed.com/posts/" + post.slug,
+
+    author: {
+      "@type": "Person",
+      name: post.author.name,
+    },
+    publisher: {
+      name: "Mora Blog",
+      "@type": "Organization",
+      url: "https://mora-ed.com",
+      logo: {
+        "@type": "ImageObject",
+        url: "https://d1ntfq67otjmwh.cloudfront.net/mora-blog-files/1717948463820-logo-small.svg",
+      },
+    },
   };
 
   return jsonLd;
