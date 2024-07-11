0d07635a73fec3f00fda2c9263825e6022a1c24f
Re9iNee <attarzadeh76@gmail.com>
2024-07-11 07:29:35 +0330

    Feature-Aboutus OG data

--- src/app/(web)/about-us/page.tsx
+++ src/app/(web)/about-us/page.tsx
@@ -6,6 +6,21 @@ import TeamSection from "@/components/about-us/team-section/team-section";
 
 export const metadata: Metadata = {
   title: "About us",
+  openGraph: {
+    // images: ""
+    siteName: "Mora Blog",
+    title: "About us | Mora Blog",
+    url: `https://www.mora-ed.com/about-us`,
+    description:
+      "Meet the MORA team and learn more about our mission and values.",
+  },
+  twitter: {
+    // images: ""
+    card: "summary",
+    title: "About us | Mora Blog",
+    description:
+      "Meet the MORA team and learn more about our mission and values.",
+  },
 };
 
 export default function AboutUsPage() {
