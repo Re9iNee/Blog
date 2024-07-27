4a521a133519154a90f4421655fbb2f2499f7653
Re9iNee <attarzadeh76@gmail.com>
2024-07-27 17:13:33 +0330

    Feature-Adding shimmer effect to the images

--- src/components/global/image-blur.tsx
+++ src/components/global/image-blur.tsx
@@ -1,42 +1,33 @@
 import Image, { ImageProps } from "next/image";
-import image1 from "public/images/post-placeholder-images/172.webp";
-import image2 from "public/images/post-placeholder-images/204.webp";
-import image3 from "public/images/post-placeholder-images/221.webp";
-import image4 from "public/images/post-placeholder-images/241.webp";
-import image5 from "public/images/post-placeholder-images/255.webp";
-import image6 from "public/images/post-placeholder-images/286.webp";
-import image7 from "public/images/post-placeholder-images/378.webp";
-import image8 from "public/images/post-placeholder-images/468.webp";
-import image9 from "public/images/post-placeholder-images/470.webp";
-import image10 from "public/images/post-placeholder-images/487.webp";
 
 export default async function BlurImage({ src, alt, ...rest }: ImageProps) {
   if (typeof src !== "string") return <Image src={src} alt={alt} {...rest} />;
-
-  const imagesPlaceholderdata = [
-    image1.blurDataURL,
-    image2.blurDataURL,
-    image3.blurDataURL,
-    image4.blurDataURL,
-    image5.blurDataURL,
-    image6.blurDataURL,
-    image7.blurDataURL,
-    image8.blurDataURL,
-    image9.blurDataURL,
-    image10.blurDataURL,
-  ];
 
   return (
     <Image
       alt={alt}
       src={src}
-      placeholder="blur"
-      blurDataURL={
-        imagesPlaceholderdata[
-          Math.floor(Math.random() * imagesPlaceholderdata.length)
-        ]
-      }
+      placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
       {...rest}
     />
   );
 }
+
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
+
+const toBase64 = (str: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
